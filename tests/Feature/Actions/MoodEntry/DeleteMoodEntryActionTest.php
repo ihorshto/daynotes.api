6bee455ab336532de43a6d83c3177f6f04d88ce3
Ihor Shtohryn <ihor.shtohrynn@gmail.com>
2025-12-25 17:09:08 +0100

    feat: enhance mood entry tests with success and failure scenarios

--- tests/Feature/Actions/MoodEntry/DeleteMoodEntryActionTest.php
+++ tests/Feature/Actions/MoodEntry/DeleteMoodEntryActionTest.php
@@ -4,11 +4,8 @@ declare(strict_types=1);
 
 use App\Models\MoodEntry;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Sanctum\Sanctum;
 use Symfony\Component\HttpFoundation\Response;
-
-uses(RefreshDatabase::class);
 
 describe('Delete Mood Entry', function (): void {
     beforeEach(function (): void {
@@ -16,7 +13,7 @@ describe('Delete Mood Entry', function (): void {
         $this->moodEntry = MoodEntry::factory()->create(['user_id' => $this->user->id]);
     });
 
-    it('delete a mood entry success', function (): void {
+    it('success in deleting mood entry', function (): void {
         Sanctum::actingAs($this->user);
 
         $response = $this->deleteJson(route('mood-entries.destroy', ['mood_entry' => $this->moodEntry->id]));
@@ -24,12 +21,18 @@ describe('Delete Mood Entry', function (): void {
         $response->assertNoContent();
     });
 
-    it('delete a mood entry fails with 403 error', function (): void {
+    it('fails in deleting, throws 403 when another user try to delete a mood entry', function (): void {
         $anotherUser = User::factory()->create();
         Sanctum::actingAs($anotherUser);
 
         $response = $this->deleteJson(route('mood-entries.destroy', ['mood_entry' => $this->moodEntry->id]));
 
         $response->assertStatus(Response::HTTP_FORBIDDEN);
+    });
+
+    it('fails in deleting, throws 401 when an unauthorized user', function (): void {
+        $response = $this->deleteJson(route('mood-entries.destroy', ['mood_entry' => $this->moodEntry->id]));
+
+        $response->assertStatus(Response::HTTP_UNAUTHORIZED);
     });
 });
