8680785f725788105b28432f45f797e4fc2cae7a
Ihor Shtohryn <ihor.shtohrynn@gmail.com>
2026-02-12 22:42:58 +0100

    feat: rename GetOneUserNotificationSettingAction to GetUserNotificationSettingAction and update method signature

--- app/Actions/UserNotificationSetting/GetUserNotificationSettingAction.php
+++ app/Actions/UserNotificationSetting/GetUserNotificationSettingAction.php
@@ -5,18 +5,14 @@ declare(strict_types=1);
 namespace App\Actions\UserNotificationSetting;
 
 use App\Models\UserNotificationSetting;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Lorisleiva\Actions\Concerns\AsController;
 
-class GetOneUserNotificationSettingAction
+class GetUserNotificationSettingAction
 {
     use AsController;
-    use AuthorizesRequests;
 
-    public function execute(UserNotificationSetting $userNotificationSetting)
+    public function handle(UserNotificationSetting $userNotificationSetting): UserNotificationSetting
     {
-        $this->authorize('view', $userNotificationSetting);
-
         return UserNotificationSetting::query()
             ->where('id', $userNotificationSetting->id)
             ->first();
