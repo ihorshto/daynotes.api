7a4f5500a10942c2c80251084c33dfb676b9d53f
Ihor Shtohryn <ihor.shtohrynn@gmail.com>
2026-02-14 12:25:39 +0100

    feat: rename notification setting factory

--- database/factories/UserNotificationSettingFactory.php
+++ database/factories/UserNotificationSettingFactory.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends Factory<UserNotificationSetting>
  */
-class NotificationSettingFactory extends Factory
+class UserNotificationSettingFactory extends Factory
 {
     /**
      * Define the model's default state.
