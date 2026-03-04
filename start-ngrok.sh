55be29fd39d8d42ac3013863945fad48876729a2
Ihor Shtohryn <ihor.shtohrynn@gmail.com>
2026-03-04 19:31:05 +0100

    feat: remove Ngrok URL update from start-ngrok.sh script

--- start-ngrok.sh
+++ start-ngrok.sh
@@ -14,9 +14,6 @@ NGROK_URL=$(curl -s http://127.0.0.1:4040/api/tunnels \
 
 echo "Ngrok URL: $NGROK_URL"
 
-# Update .env
-sed -i.bak "s|^NGROK_URL=.*|NGROK_URL=$NGROK_URL|" .env
-
 # Clear config cache
 php artisan config:clear
 
